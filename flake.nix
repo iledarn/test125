659c4880ca69450c837f722f0af556af6ea3cdb6
Ildar Nasyrov <iledarnp@gmail.com>
2024-09-21 02:40:53 +0800

    trying to fix installing pillow - error: numpy-1.20.2 not supported for interpreter python3.6

--- flake.nix
+++ flake.nix
@@ -13,6 +13,25 @@
 
     pythonEnv = pkgs.python36.withPackages (ps: with ps; [
       requests
+      Babel
+      pypdf2
+      passlib
+      werkzeug
+      lxml
+      decorator
+      dateutil
+      psycopg2
+      (pillow.overridePythonAttrs (old: {
+        propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ [
+          (numpy.overridePythonAttrs (oldAttrs: rec {
+            version = "1.19.5";
+            src = oldAttrs.src.override {
+              inherit version;
+              sha256 = "0fdbaa32c9eb09ef09d425dc154628fca6fa69d2f7c1a33f889abb7e0efb3909";
+            };
+          }))
+        ];
+      }))
     ]);
 
     myEnv = pkgs.buildEnv {
