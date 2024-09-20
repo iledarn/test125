83fcdee02176e02a61761443ed095ea4789637fe
Ildar Nasyrov <iledarnp@gmail.com>
2024-09-21 03:36:08 +0800

    21.05 again - need postgresql 13

--- flake.nix
+++ flake.nix
@@ -2,16 +2,17 @@
   description = "Odoo v12.0 nix-od environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-20.09";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-21.05";
     flake-utils.url = "github:numtide/flake-utils";
+    wkhtmltopdf-flake.url = "github:iledarn/wkhtmltopodfnix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, wkhtmltopdf-flake }:
   flake-utils.lib.eachDefaultSystem (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
 
-    pythonEnv = pkgs.python36.withPackages (ps: with ps; [
+    pythonEnv = pkgs.python37.withPackages (ps: with ps; [
       requests
       Babel
       pypdf2
@@ -22,12 +23,23 @@
       dateutil
       psycopg2
       pillow
+      setuptools
+      psutil
+      jinja2
+      reportlab
+      html2text
+      docutils
+      num2words
     ]);
+
+    wkhtmltopdf = wkhtmltopdf-flake.packages.${system}.default;
 
     myEnv = pkgs.buildEnv {
       name = "my-python-env-3-6";
       paths = with pkgs; [
         pythonEnv
+        postgresql_13
+        wkhtmltopdf
       ];
     };
   in
