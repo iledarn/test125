445d624a3c9620ca57ceb8a505a7400e14a675f8
Ildar Nasyrov <iledarnp@gmail.com>
2024-09-20 14:05:00 +0800

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Odoo v12.0 nix-od environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    pythonEnv = pkgs.python36.withPackages (ps: with ps; [
+      requests
+    ]);
+
+    myEnv = pkgs.buildEnv {
+      name = "my-python-env-3-6-";
+      paths = with pkgs; [
+        pythonEnv
+      ];
+    };
+  in
+  {
+    packages.default = myEnv;
+
+    apps.default = {
+      type = "app";
+      program = "${myEnv}/bin/python";
+    };
+  }
+  );
+}
